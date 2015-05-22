f937143aacdb72e345ad280f6da19c98487b3657
ka-s <using0namespace0kas@gmail.com>
2015-05-23 01:50:04 +0900

    Direct3DObjectファイルを追加.
    Direct3DObjectクラスを作成.

---
+++ DirectXToolKit_my_library/src/Direct3DObject.cpp
@@ -0,0 +1,12 @@
+//
+// Direct3DObject.cpp
+//
+
+// @author ka-s
+
+#include "pch.h"
+#include "Direct3DObject.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
---
+++ DirectXToolKit_my_library/src/Direct3DObject.h
@@ -0,0 +1,17 @@
+//
+// Direct3DObject.h
+//
+
+// @author ka-s
+
+#pragma once
+
+#include "pch.h"
+
+class Direct3DObject
+{
+private:
+
+public:
+
+};
